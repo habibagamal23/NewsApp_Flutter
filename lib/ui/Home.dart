f2b83861275bc17daf38eebbc8a18fd253eb1cae
habibagamal23 <habiba.gamaall23@gmail.com>
2021-07-13 19:10:46 +0200

    HomePage

---
+++ lib/ui/Home.dart
@@ -0,0 +1,64 @@
+
+import 'package:flutter/material.dart';
+import 'package:news_app/main.dart';
+
+import 'CatogariItem.dart';
+
+class Home extends StatelessWidget {
+
+static final Routename = "home";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      appBar:  AppBar(
+
+        title: Text('News App '),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20))
+          ),
+        ),
+
+body: ListView.builder(
+  itemCount: 6,
+    padding: const EdgeInsets.all(8),
+    itemBuilder: (BuildContext context, int index) {
+      return Container(
+        child: Column(
+          children: [
+            Text("Pick your Categary "),
+            Row(
+              children: [
+                CatograiItem("assets/image/ball.png", "sport", false , myThem.red),
+
+                CatograiItem("assets/image/Politics.png", "Politics", true , myThem.blue),
+              ],
+
+            ),
+            Row(
+              children: [
+                CatograiItem("assets/image/health.png", "Health", false , myThem.bink),
+
+                CatograiItem("assets/image/bussines.png", "Bussines", true , myThem.brown),
+              ],
+
+            ),
+            Row(
+              children: [
+                CatograiItem("assets/image/environment.png", "Environment", false , myThem.red),
+
+                CatograiItem("assets/image/science.png", "Science", true , myThem.blue),
+              ],
+
+            ),
+          ],
+        ),
+
+      );
+
+    }
+),
+    );
+  }
+}
